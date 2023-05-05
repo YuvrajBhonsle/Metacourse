bdb7d07fa75aa331ebebd1bc00c634932962fe24
Yuvraj Bhonsle <coding.fun103@gmail.com>
2023-05-05 21:41:22 +0530

    Hero Page and Route Pages Added

---
+++ pages/branch/[num].tsx
@@ -0,0 +1,260 @@
+import { Grid, Box, useColorModeValue, Img, Heading } from "@chakra-ui/react";
+
+const SemNum = () => {
+  return (
+    <Box m={"3rem"}>
+      <Grid templateColumns="repeat(2, 1fr)" gap={8} alignItems={"center"}>
+        <Box
+          bg={useColorModeValue("white","blackAlpha.600")}
+          color={useColorModeValue("black", "white")}
+          textAlign={"center"}
+          p={5}
+          rounded={"md"}
+          border={"1px"}
+          borderColor="black"
+          boxShadow={useColorModeValue("6px 6px 0 black", "6px 6px 0 #656665")}
+          _hover={{
+            // background: "white",
+            color: "#606263",
+            boxShadow: useColorModeValue("6px 6px 0 gray", "6px 6px 0 white"),
+            transition: "all 0.3s ease-out",
+            transform: "translateY(-8px)",
+          }}
+        >
+          <Box h={"200px"} borderBottom={"1px"} borderColor="black">
+            <Img
+              src={
+                "https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+              }
+              roundedTop={"sm"}
+              objectFit="cover"
+              h="full"
+              w="full"
+              alt={"Blog Image"}
+            />
+          </Box>
+          <Heading mt={"3"}>SEMESTER 1</Heading>
+        </Box>
+        <Box
+          bg={useColorModeValue("white","blackAlpha.600")}
+          color={useColorModeValue("black", "white")}
+          textAlign={"center"}
+          p={5}
+          rounded={"md"}
+          border={"1px"}
+          borderColor="black"
+          boxShadow={useColorModeValue("6px 6px 0 black", "6px 6px 0 #656665")}
+          _hover={{
+            // background: "white",
+            color: "#606263",
+            boxShadow: useColorModeValue("6px 6px 0 gray", "6px 6px 0 white"),
+            transition: "all 0.3s ease-out",
+            transform: "translateY(-8px)",
+          }}
+        >
+          <Box h={"200px"} borderBottom={"1px"} borderColor="black">
+            <Img
+              src={
+                "https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+              }
+              roundedTop={"sm"}
+              objectFit="cover"
+              h="full"
+              w="full"
+              alt={"Blog Image"}
+            />
+          </Box>
+          <Heading mt={"3"}>SEMESTER 2</Heading>
+        </Box>
+        <Box
+          bg={useColorModeValue("white","blackAlpha.600")}
+          color={useColorModeValue("black", "white")}
+          textAlign={"center"}
+          p={5}
+          rounded={"md"}
+          border={"1px"}
+          borderColor="black"
+          boxShadow={useColorModeValue("6px 6px 0 black", "6px 6px 0 #656665")}
+          _hover={{
+            // background: "white",
+            color: "#606263",
+            boxShadow: useColorModeValue("6px 6px 0 gray", "6px 6px 0 white"),
+            transition: "all 0.3s ease-out",
+            transform: "translateY(-8px)",
+          }}
+        >
+          <Box h={"200px"} borderBottom={"1px"} borderColor="black">
+            <Img
+              src={
+                "https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+              }
+              roundedTop={"sm"}
+              objectFit="cover"
+              h="full"
+              w="full"
+              alt={"Blog Image"}
+            />
+          </Box>
+          <Heading mt={"3"}>SEMESTER 3</Heading>
+        </Box>
+        <Box
+          bg={useColorModeValue("white","blackAlpha.600")}
+          color={useColorModeValue("black", "white")}
+          textAlign={"center"}
+          p={5}
+          rounded={"md"}
+          border={"1px"}
+          borderColor="black"
+          boxShadow={useColorModeValue("6px 6px 0 black", "6px 6px 0 #656665")}
+          _hover={{
+            // background: "white",
+            color: "#606263",
+            boxShadow: useColorModeValue("6px 6px 0 gray", "6px 6px 0 white"),
+            transition: "all 0.3s ease-out",
+            transform: "translateY(-8px)",
+          }}
+        >
+          <Box h={"200px"} borderBottom={"1px"} borderColor="black">
+            <Img
+              src={
+                "https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+              }
+              roundedTop={"sm"}
+              objectFit="cover"
+              h="full"
+              w="full"
+              alt={"Blog Image"}
+            />
+          </Box>
+          <Heading mt={"3"}>SEMESTER 4</Heading>
+        </Box>
+        <Box
+          bg={useColorModeValue("white","blackAlpha.600")}
+          color={useColorModeValue("black", "white")}
+          textAlign={"center"}
+          p={5}
+          rounded={"md"}
+          border={"1px"}
+          borderColor="black"
+          boxShadow={useColorModeValue("6px 6px 0 black", "6px 6px 0 #656665")}
+          _hover={{
+            // background: "white",
+            color: "#606263",
+            boxShadow: useColorModeValue("6px 6px 0 gray", "6px 6px 0 white"),
+            transition: "all 0.3s ease-out",
+            transform: "translateY(-8px)",
+          }}
+        >
+          <Box h={"200px"} borderBottom={"1px"} borderColor="black">
+            <Img
+              src={
+                "https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+              }
+              roundedTop={"sm"}
+              objectFit="cover"
+              h="full"
+              w="full"
+              alt={"Blog Image"}
+            />
+          </Box>
+          <Heading mt={"3"}>SEMESTER 5</Heading>
+        </Box>
+        <Box
+          bg={useColorModeValue("white","blackAlpha.600")}
+          color={useColorModeValue("black", "white")}
+          textAlign={"center"}
+          p={5}
+          rounded={"md"}
+          border={"1px"}
+          borderColor="black"
+          boxShadow={useColorModeValue("6px 6px 0 black", "6px 6px 0 #656665")}
+          _hover={{
+            // background: "white",
+            color: "#606263",
+            boxShadow: useColorModeValue("6px 6px 0 gray", "6px 6px 0 white"),
+            transition: "all 0.3s ease-out",
+            transform: "translateY(-8px)",
+          }}
+        >
+          <Box h={"200px"} borderBottom={"1px"} borderColor="black">
+            <Img
+              src={
+                "https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+              }
+              roundedTop={"sm"}
+              objectFit="cover"
+              h="full"
+              w="full"
+              alt={"Blog Image"}
+            />
+          </Box>
+          <Heading mt={"3"}>SEMESTER 6</Heading>
+        </Box>
+        <Box
+          bg={useColorModeValue("white","blackAlpha.600")}
+          color={useColorModeValue("black", "white")}
+          textAlign={"center"}
+          p={5}
+          rounded={"md"}
+          border={"1px"}
+          borderColor="black"
+          boxShadow={useColorModeValue("6px 6px 0 black", "6px 6px 0 #656665")}
+          _hover={{
+            // background: "white",
+            color: "#606263",
+            boxShadow: useColorModeValue("6px 6px 0 gray", "6px 6px 0 white"),
+            transition: "all 0.3s ease-out",
+            transform: "translateY(-8px)",
+          }}
+        >
+          <Box h={"200px"} borderBottom={"1px"} borderColor="black">
+            <Img
+              src={
+                "https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+              }
+              roundedTop={"sm"}
+              objectFit="cover"
+              h="full"
+              w="full"
+              alt={"Blog Image"}
+            />
+          </Box>
+          <Heading mt={"3"}>SEMESTER 7</Heading>
+        </Box>
+        <Box
+          bg={useColorModeValue("white","blackAlpha.600")}
+          color={useColorModeValue("black", "white")}
+          textAlign={"center"}
+          p={5}
+          rounded={"md"}
+          border={"1px"}
+          borderColor="black"
+          boxShadow={useColorModeValue("6px 6px 0 black", "6px 6px 0 #656665")}
+          _hover={{
+            // background: "white",
+            color: "#606263",
+            boxShadow: useColorModeValue("6px 6px 0 gray", "6px 6px 0 white"),
+            transition: "all 0.3s ease-out",
+            transform: "translateY(-8px)",
+          }}
+        >
+          <Box h={"200px"} borderBottom={"1px"} borderColor="black">
+            <Img
+              src={
+                "https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+              }
+              roundedTop={"sm"}
+              objectFit="cover"
+              h="full"
+              w="full"
+              alt={"Blog Image"}
+            />
+          </Box>
+          <Heading mt={"3"}>SEMESTER 8</Heading>
+        </Box>
+      </Grid>
+    </Box>
+  );
+};
+
+export default SemNum;
