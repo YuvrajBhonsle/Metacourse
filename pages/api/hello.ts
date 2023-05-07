e4aafb16b7e8cb4c44b5c630ffc437cad1b53dbf
VIVEK-SUTHAR <sutharvivek18@gmail.com>
2023-05-07 20:42:16 +0530

    fea:basic api example

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    res.status(403).json({ message: "Method Not allowed" });
+  }
+  try {
+    const { id } = req.body;
+    res.status(200).json({ name: `${id}-from metacourse api` });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
