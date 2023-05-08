1da605576a5e87220ca13618b3794366007f93d2
Yuvraj Bhonsle <coding.fun103@gmail.com>
2023-05-08 11:53:24 +0530

    Dynamic Text lettercase and links modified

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,45 +1,53 @@
-import { APP_NAME } from "@/constants";
-import { Center, Flex, Grid, Box } from "@chakra-ui/react";
-import { HeroCards } from "./HeroCards";
+import { APP_NAME } from '@/constants';
+import { Center, Flex, Grid, Box } from '@chakra-ui/react';
+import { HeroCards } from './HeroCards';
+import Navbar from './Navbar';
 
 export default function Hero() {
-  const course = ["B.E", "B.Com", "B.Arch", ""];
+  const course = ['B.E', 'B.Com', 'B.Arch', ''];
   const branch = [
-    "AERONAUTICAL ENGINEERING",
-    "MECHANICAL ENGINEERING",
-    "CIVIL ENGINEERING",
-    "COMPUTER ENGINEERING",
-    "ELECTRICAL ENGINEERING",
-    "INFORMATION TECHNOLOGY",
-    "IC ENGINEERING",
-    "EC ENGINEERING",
-    "MCA",
+    'Aeronautical-Engineering',
+    'Mechanical-Engineering',
+    'Civil-Engineering',
+    'Computer-Engineering',
+    'Electrical-Engineering',
+    'Information-Technology',
+    'IC-Engineering',
+    'EC-Engineering',
+    'MCA',
   ];
 
+  // {
+  //   branch.map((name, key) => {
+  //     <Navbar
+  //       props={name
+  //         .split('-')
+  //         .map((word, index) => word[0].toUpperCase() + word.slice(1))
+  //         .join('')}
+  //     />;
+  //   });
+  // }
   return (
-    <Box w={"100vw"}>
-      <Center
-        fontSize={"3xl"}
-        fontWeight={"bold"}
-        mt={"5"}
-        mb={"2"}
-        letterSpacing={"wide"}
-      >
+    <Box w={'100vw'}>
+      <Center fontSize={'3xl'} fontWeight={'bold'} mt={'5'} mb={'2'} letterSpacing={'wide'}>
         Welcome to {APP_NAME}
       </Center>
 
       <Flex
-        flexWrap={"wrap"}
-        justifyContent={"center"}
-        alignItems={"stretch"}
+        flexWrap={'wrap'}
+        justifyContent={'center'}
+        alignItems={'stretch'}
         rowGap={4}
         columnGap={8}
       >
         {branch.map((name, key) => (
           <HeroCards
-            props={name}
+            props={name
+              .split('-')
+              .map((word, index) => word[0].toUpperCase() + word.slice(1))
+              .join(' ')}
             key={key}
-            id={name.split(" ").join("").toLowerCase()}
+            id={name.split('-').join(' ')}
           />
         ))}
       </Flex>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import {
   Avatar,
   Box,
@@ -14,55 +14,61 @@ import {
   Stack,
   useColorMode,
   useColorModeValue,
-} from "@chakra-ui/react";
-import { useRouter } from "next/router";
+} from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 
-export default function Navbar({ props }: any) {
+export default function Navbar() {
   const { colorMode, toggleColorMode } = useColorMode();
   const router = useRouter();
 
   const getRouteName = () => {
-    if (router.pathname.includes("branch")) {
-      return router.query.branch;
-    } else return "Home";
-    // MORE ROUTES CAN VE DEFINED HERE
+    if (router.pathname.includes('branch')) {
+      // return {props} as any;
+      // return router.query.branch;
+      let route: string = router.query.branch as string;
+      return route
+        .split('%20')
+        .map((word, index) => word[0].toUpperCase() + word.slice(1))
+        .join(' ');
+    } else return 'Home';
+    // MORE ROUTES CAN BE DEFINED HERE
   };
 
   return (
     <>
-      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
-        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <Box w={"10%"} alignItems={"center"} bg={"white"}>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <Box w={'10%'} alignItems={'center'} bg={'white'}>
             <Image src="/next.svg" />
           </Box>
 
-          <Box fontSize={"xl"}>{getRouteName()}</Box>
-          <Flex alignItems={"center"}>
-            <Stack direction={"row"} spacing={7}>
+          <Box fontSize={'xl'}>{getRouteName()}</Box>
+          <Flex alignItems={'center'}>
+            <Stack direction={'row'} spacing={7}>
               <Button onClick={toggleColorMode}>
-                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
               </Button>
 
               <Menu>
                 <MenuButton
                   as={Button}
-                  rounded={"full"}
-                  variant={"link"}
-                  cursor={"pointer"}
+                  rounded={'full'}
+                  variant={'link'}
+                  cursor={'pointer'}
                   minW={0}
                 >
                   <Avatar
-                    size={"sm"}
-                    src={"https://avatars.dicebear.com/api/male/username.svg"}
+                    size={'sm'}
+                    src={'https://avatars.dicebear.com/api/male/username.svg'}
                     // isOpen={isOpen} onClose={onClose} onOverlayClick={onClose}
                   />
                 </MenuButton>
-                <MenuList alignItems={"center"}>
+                <MenuList alignItems={'center'}>
                   <br />
                   <Center>
                     <Avatar
-                      size={"2xl"}
-                      src={"https://avatars.dicebear.com/api/male/username.svg"}
+                      size={'2xl'}
+                      src={'https://avatars.dicebear.com/api/male/username.svg'}
                     />
                   </Center>
                   <br />
