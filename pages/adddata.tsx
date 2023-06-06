21cf999fb3064d54723c20cb09d7241937a5ab19
Yuvraj Bhonsle <yuvrajyhb@gmail.com>
2023-06-06 22:24:21 +0530

    fix: fixed semester type from string to number

--- pages/adddata.tsx
+++ pages/adddata.tsx
@@ -11,14 +11,14 @@ export default function addData() {
   const toast = useToast();
 
   const options = [
-    { label: 'Semester 1', value: '1' },
-    { label: 'Semester 2', value: '2' },
-    { label: 'Semester 3', value: '3' },
-    { label: 'Semester 4', value: '4' },
-    { label: 'Semester 5', value: '5' },
-    { label: 'Semester 6', value: '6' },
-    { label: 'Semester 7', value: '7' },
-    { label: 'Semester 8', value: '8' },
+    { label: 'Semester 1', value: 1 },
+    { label: 'Semester 2', value: 2 },
+    { label: 'Semester 3', value: 3 },
+    { label: 'Semester 4', value: 4 },
+    { label: 'Semester 5', value: 5 },
+    { label: 'Semester 6', value: 6 },
+    { label: 'Semester 7', value: 7 },
+    { label: 'Semester 8', value: 8 },
   ];
 
   const branches = [
