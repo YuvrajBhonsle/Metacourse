8451224a07a5b37e9d0f5cf0ec5b2d4ce559cdb8
VIVEK-SUTHAR <sutharvivek18@gmail.com>
2023-06-27 19:06:07 +0530

    images

--- constants/index.ts
+++ constants/index.ts
@@ -5,3 +5,8 @@ export const APP_DESC = "Metacourse ka abstract or description";
 
 export const IMAGE_LINK =
   "https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80";
+
+export const SEM_IMAGES = [
+  'https://media.istockphoto.com/id/1143248561/vector/number-1-red-neon-light-on-black-wall.jpg?s=612x612&w=0&k=20&c=C8JF7-R9H7FlLcMLVDMK69NEz4JvxXtcerr7TuvC1H8=',
+  'https://www.shutterstock.com/image-illustration/3d-render-number-two-glowing-260nw-1890767737.jpg',
+];
--- pages/branch/[branch].tsx
+++ pages/branch/[branch].tsx
@@ -1,17 +1,12 @@
 'use client';
 import { useRouter } from 'next/router';
-import { IMAGE_LINK } from "@/constants";
-import { Box, Flex, Heading, Img, useColorModeValue } from "@chakra-ui/react";
+import { IMAGE_LINK, SEM_IMAGES } from '@/constants';
+import { Box, Flex, Heading, Img, useColorModeValue } from '@chakra-ui/react';
 
 const SemNum = () => {
   return (
-    <Box m={"8"}>
-      <Flex
-        flexWrap={"wrap"}
-        justifyContent={"center"}
-        alignItems={"center"}
-        gap={[8, 16]}
-      >
+    <Box m={'8'}>
+      <Flex flexWrap={'wrap'} justifyContent={'center'} alignItems={'center'} gap={[8, 16]}>
         {[...Array(8)].map((_, index) => (
           <SemesterCard key={index} number={index} />
         ))}
@@ -20,50 +15,51 @@ const SemNum = () => {
   );
 };
 
-// export default getPathName = () => {
-//   const pathName = `${number+1}_${router.query.branch}`;
-// }
-
 const SemesterCard = ({ number }: { number: number }) => {
   const router = useRouter();
-  
 
   return (
     <Box
-      bg={useColorModeValue("white", "blackAlpha.600")}
-      color={useColorModeValue("black", "white")}
-      textAlign={"center"}
+      bg={useColorModeValue('white', 'blackAlpha.600')}
+      color={useColorModeValue('black', 'white')}
+      textAlign={'center'}
       p={5}
       alignSelf="center"
-      rounded={"md"}
-      border={"1px"}
+      rounded={'md'}
+      border={'1px'}
       borderColor="black"
-      boxShadow={useColorModeValue("6px 6px 0 black", "6px 6px 0 #656665")}
+      boxShadow={useColorModeValue('6px 6px 0 black', '6px 6px 0 #656665')}
       _hover={{
         // background: "white",
-        color: "#606263",
-        boxShadow: useColorModeValue("6px 6px 0 gray", "6px 6px 0 white"),
-        transition: "all 0.3s ease-out",
-        transform: "translateY(-8px)",
+        color: '#606263',
+        boxShadow: useColorModeValue('6px 6px 0 gray', '6px 6px 0 white'),
+        transition: 'all 0.3s ease-out',
+        transform: 'translateY(-8px)',
       }}
     >
-      <Box h={"200px"} borderBottom={"1px"} borderColor="black">
+      <Box h={'200px'} borderBottom={'1px'} borderColor="black">
         <Img
-          src={IMAGE_LINK}
-          roundedTop={"sm"}
+          src={SEM_IMAGES[0]}
+          roundedTop={'sm'}
           objectFit="cover"
           h="full"
           w="full"
-          alt={"Blog Image"}
+          alt={'Blog Image'}
         />
       </Box>
-      <Heading mt={"3"} onClick={() => {
-        console.log(number + 1) 
-        console.log(router.pathname)
-        console.log(router.query.branch)
-        console.log(`${number+1}_${router.query.branch}`)
-        router.push(`/data/${number+1}_${router.query.branch}`)
-        }} cursor={"pointer"}>Semester {number + 1}</Heading>
+      <Heading
+        mt={'3'}
+        onClick={() => {
+          // console.log(number + 1);
+          // console.log(router.pathname);
+          // console.log(router.query.branch);
+          // console.log(`${number + 1}_${router.query.branch}`);
+          router.push(`/data/${number + 1}_${router.query.branch}`);
+        }}
+        cursor={'pointer'}
+      >
+        Semester {number + 1}
+      </Heading>
     </Box>
   );
 };
